8403d1e2d00b773a4778a650862c764c3b9c299b
dbhagrav <61030008+dbhagrav@users.noreply.github.com>
2024-01-24 15:29:44 -0800

    HR

---
+++ sql/BQ/select_results.sql
@@ -0,0 +1,1 @@
+SELECT * FROM `gcp-edwprddata-prd-33200.DCFRMWRKSNDDB.MC_DATA_CONTRACTS_SCHEMA_RESULTS` where monitor_name like '%HR%' and trx_id_name = 'STATEMENT_OF_WORK_ID'
---
+++ sql/BQ/update_header_timestamp.sql
@@ -0,0 +1,1 @@
+update `gcp-edwprddata-prd-33200.DCFRMWRKSNDDB.MC_DC_SCHEMA_HEADER_INFO` set LAST_VALIDATION_DATE = TIMESTAMP_SUB(current_timestamp(), INTERVAL 1 DAY) where TRUE
