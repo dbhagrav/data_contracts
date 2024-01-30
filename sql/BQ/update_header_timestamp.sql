e0b39918b32941773c5dbfcc3544812688bcc81c
dbhagrav <61030008+dbhagrav@users.noreply.github.com>
2024-01-30 10:51:35 -0800

    test

--- sql/BQ/update_header_timestamp.sql
+++ sql/BQ/update_header_timestamp.sql
@@ -1,1 +1,1 @@
-update `gcp-edwprddata-prd-33200.DCFRMWRKSNDDB.MC_DC_SCHEMA_HEADER_INFO` set LAST_VALIDATION_DATE = TIMESTAMP_SUB(current_timestamp(), INTERVAL 1 DAY) where data_domain = 'TEST'
+update `gcp-edwprddata-prd-33200.DCFRMWRKSNDDB.MC_DC_SCHEMA_HEADER_INFO` set LAST_VALIDATION_DATE = TIMESTAMP_SUB(current_timestamp(), INTERVAL 7 DAY) where data_domain = 'HR'
