f1d3e0b0f8c2064232435d2ade2be1f264338bff
dbhagrav <61030008+dbhagrav@users.noreply.github.com>
2024-02-01 13:44:27 -0800

    sales

---
+++ sql/BQ/DC_TEST_INVALID_PRODUCT_PRC.sql
@@ -0,0 +1,9 @@
+create or replace procedure `gcp-edwprddata-prd-33200`.DCFRMWRKSNDDB.DC_TEST_INVALID_PRODUCT_PRC(fromdate TIMESTAMP)
+  BEGIN
+    SELECT "Semantic Rules - Check PRODUCT_ID Validity" as MONITOR_NAME, CONCAT("Ordered Item:", ORDERED_ITEM) as VALIDATION_ERROR_MESSAGE, LINE_ID as TRX_ID, TO_JSON_STRING(t) AS SOURCE_SIMILAR_DATA from
+    (select ORDERED_ITEM, LINE_ID
+    FROM `gcp-edwprddata-prd-33200`.TRANS_NRTNCRDB.ST_SI_XXOPL_ORDER_LINES_ALL AS source LEFT JOIN `gcp-edwprddata-prd-33200`.BR_REFERENCEDB.N_PRODUCT AS target ON source.`ORDERED_ITEM` = target.`BK_PRODUCT_ID` WHERE NOT source.`ORDERED_ITEM` IS NULL AND target.`BK_PRODUCT_ID` IS NULL and source.EDWBQ_CREATE_DTM > fromdate ) t;
+  END;
+
+
+call `gcp-edwprddata-prd-33200`.DCFRMWRKSNDDB.DC_TEST_INVALID_PRODUCT_PRC('2018-01-01 00:00:00');
