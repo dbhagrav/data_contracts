6212550f571bf0c9e19461649f62cbd7242d615e
dbhagrav <61030008+dbhagrav@users.noreply.github.com>
2024-02-12 07:39:26 -0800

    changed monitor_type

--- sql/BQ/select_results.sql
+++ sql/BQ/select_results.sql
@@ -1,2 +1,8 @@
-SELECT * FROM `gcp-edwprddata-prd-33200.DCFRMWRKSNDDB.MC_DATA_CONTRACTS_SCHEMA_RESULTS` where monitor_name like '%HR%' and trx_id_name = 'STATEMENT_OF_WORK_ID'
+SELECT * FROM `gcp-edwprddata-prd-33200.DCFRMWRKSNDDB.MC_DATA_CONTRACTS_SCHEMA_RESULTS` where monitor_name like '%HR%' and table_name like '%FPA_SOW_DETAIL'
+SELECT * FROM `gcp-edwprddata-prd-33200.DCFRMWRKSNDDB.MC_DATA_CONTRACTS_SCHEMA_RESULTS` where monitor_name like '%HR%' and table_name like '%FPA_JOB_POSTING_DETAIL'
+SELECT * FROM `gcp-edwprddata-prd-33200.DCFRMWRKSNDDB.MC_DATA_CONTRACTS_SCHEMA_RESULTS` where monitor_name like '%HR%' and table_name like '%FPA_SOW_DETAIL'
+SELECT * FROM `gcp-edwprddata-prd-33200.DCFRMWRKSNDDB.MC_DATA_CONTRACTS_SCHEMA_RESULTS` where monitor_name like '%HR%' and table_name like '%FPA_SOW_LINE_ITEM_REVIEW'
+SELECT * FROM `gcp-edwprddata-prd-33200.DCFRMWRKSNDDB.MC_DATA_CONTRACTS_SCHEMA_RESULTS` where monitor_name like '%HR%' and table_name like '%FPA_WORK_ORDER_DETAIL'
+
+
 select * from `gcp-edwprddata-prd-33200.DCFRMWRKSNDDB.MC_DATA_CONTRACTS_SCHEMA_RESULTS` where monitor_name like '%HR%edwprddata-prd-33200%'
