6fda4c4b12532ef700a245a9a0637f80ecfa10c2
dbhagrav <61030008+dbhagrav@users.noreply.github.com>
2024-02-19 08:44:56 -0800

    updarted finance contractr

---
+++ sql/BQ (2)/select_results.sql
@@ -0,0 +1,12 @@
+SELECT * FROM `gcp-edwprddata-prd-33200.DCFRMWRKSNDDB.MC_DATA_CONTRACTS_SCHEMA_RESULTS` where monitor_name like '%HR%' and table_name like '%FPA_SOW_DETAIL'
+SELECT * FROM `gcp-edwprddata-prd-33200.DCFRMWRKSNDDB.MC_DATA_CONTRACTS_SCHEMA_RESULTS` where monitor_name like '%HR%' and table_name like '%FPA_JOB_POSTING_DETAIL'
+SELECT * FROM `gcp-edwprddata-prd-33200.DCFRMWRKSNDDB.MC_DATA_CONTRACTS_SCHEMA_RESULTS` where monitor_name like '%HR%' and table_name like '%FPA_SOW_DETAIL'
+SELECT * FROM `gcp-edwprddata-prd-33200.DCFRMWRKSNDDB.MC_DATA_CONTRACTS_SCHEMA_RESULTS` where monitor_name like '%HR%' and table_name like '%FPA_SOW_LINE_ITEM_REVIEW'
+SELECT * FROM `gcp-edwprddata-prd-33200.DCFRMWRKSNDDB.MC_DATA_CONTRACTS_SCHEMA_RESULTS` where monitor_name like '%HR%' and table_name like '%FPA_WORK_ORDER_DETAIL'
+
+
+select * from `gcp-edwprddata-prd-33200.DCFRMWRKSNDDB.MC_DATA_CONTRACTS_SCHEMA_RESULTS` where monitor_name like '%HR%edwprddata-prd-33200%'
+
+
+
+SELECT * FROM `gcp-edwprddata-prd-33200.DCFRMWRKSNDDB.MC_DATA_CONTRACTS_SCHEMA_RESULTS` where table_name ='FPA_SOW_DETAIL' and edw
