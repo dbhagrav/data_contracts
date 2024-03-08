3c09a770678e46ac13a1ddd48e7e6cf0aae8b1c2
dbhagrav <61030008+dbhagrav@users.noreply.github.com>
2024-03-08 05:30:14 -0800

    changed last_updater_date

--- sql/BQ/update_header_timestamp.sql
+++ sql/BQ/update_header_timestamp.sql
@@ -1,1 +1,1 @@
-update `gcp-edwprddata-prd-33200.DCFRMWRKSNDDB.MC_DC_SCHEMA_HEADER_INFO` set LAST_VALIDATION_DATE = TIMESTAMP_SUB(current_timestamp(), INTERVAL 3000 DAY) where data_domain = 'SALES' and table_name = 'ST_SI_XXOPL_ORDER_LINES_ALL'
+update `gcp-edwprddata-prd-33200.DCFRMWRKSNDDB.MC_DC_SCHEMA_HEADER_INFO` set LAST_VALIDATION_DATE = TIMESTAMP_SUB(current_timestamp(), INTERVAL 3000 DAY) where data_domain = 'FINANCE' and monitor_trype = 'BQ_SEMANTIC'
