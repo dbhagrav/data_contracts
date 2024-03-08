43e54c4ba5a2877fddccc9fd7f5d4b83e266d8f9
dbhagrav <61030008+dbhagrav@users.noreply.github.com>
2024-03-08 05:59:36 -0800

    changed SOR

--- sql/BQ/DC_TEST_INVALID_PRODUCT_PRC.sql
+++ sql/BQ/DC_TEST_INVALID_PRODUCT_PRC.sql
@@ -1,7 +1,7 @@
 create or replace procedure `gcp-edwprddata-prd-33200`.DCFRMWRKSNDDB.DC_TEST_INVALID_PRODUCT_PRC(fromdate TIMESTAMP)
   BEGIN
     SELECT "Semantic Rules - Check PRODUCT_ID Validity" as MONITOR_NAME, CONCAT("Invalid Ordered Item:", ORDERED_ITEM, ". Corresponding BK_PRODUCT_ID in EDW Refeence table BR_REFERENCEDB.N_PRODUCT is null") as VALIDATION_ERROR_MESSAGE, LINE_ID as TRX_ID, TO_JSON_STRING(t) AS SOURCE_SIMILAR_DATA from
-    (select ORDERED_ITEM, LINE_ID
+    (select ORDERED_ITEM, LINE_ID, EDWBQ_CREATE_DTM
     FROM `gcp-edwprddata-prd-33200`.TRANS_NRTNCRDB.ST_SI_XXOPL_ORDER_LINES_ALL AS source LEFT JOIN `gcp-edwprddata-prd-33200`.BR_REFERENCEDB.N_PRODUCT AS target ON source.`ORDERED_ITEM` = target.`BK_PRODUCT_ID` WHERE NOT source.`ORDERED_ITEM` IS NULL AND target.`BK_PRODUCT_ID` IS NULL and source.EDWBQ_CREATE_DTM > fromdate ) t;
   END;
 
